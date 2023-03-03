50de92bd5784c7ab7913d33851c904e53e538a31
Ikaro Felix <ikaroisdm@gmail.com>
2023-03-03 20:18:35 -0300

    GitHub Pages

---
+++ js/api.js
@@ -0,0 +1,7 @@
+const API_KEY = "a2e4f4441881c2771c11167f68b27877"
+
+// After deployment, the api key is shown to the client
+// but there is no need to work on that, since it's not
+// that important to hide it on this project
+
+export default API_KEY
